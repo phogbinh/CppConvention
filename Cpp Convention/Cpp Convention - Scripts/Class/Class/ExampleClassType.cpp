20635baf6135dd7c4a0646f9a30c63af7ec45c31
陳風平 <32406100+phogbinh@users.noreply.github.com>
2018-11-15 23:02:19 +0800

    Update 1.1
    Modified
    1. Cpp Convention - Documents
    2. Cpp Convention - Images > ExampleClassTypeDotCPP.psd
    3. Cpp Convention - Images > ExampleClassTypeDotH.psd
    4. Cpp Convention - Scripts > Class > Class.sln
    
    Created
    1. README.txt
    1. Cpp Convention - Images > main() Definition.psd

--- Cpp Convention/Cpp Convention - Scripts/Class/Class/ExampleClassType.cpp
+++ Cpp Convention/Cpp Convention - Scripts/Class/Class/ExampleClassType.cpp
@@ -3,20 +3,115 @@ This is the source code file 'ExampleClassType.cpp'. This is the implementation 
 'ExampleClassType'. */
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 #include "ExampleClassType.h"
 
-ExampleClassType::ExampleClassType() : ExampleClassType(0)
+ExampleClassType::~ExampleClassType()
 {
     /* Body intentionally empty */
 }
 
-ExampleClassType::ExampleClassType(int dataValue) : _data(dataValue)
+ExampleClassType::ExampleClassType() : ExampleClassType(vector<int> {})
 {
-    if (_data < 0)
+    /* Body intentionally empty */
+}
+
+ExampleClassType::ExampleClassType(const ExampleClassType& objectValue) : ExampleClassType(objectValue._data)
+{
+    /* Body intentionally empty */
+}
+
+ExampleClassType::ExampleClassType(const vector<int>& dataValue) : _data(dataValue)
+{
+    if (_data.size() < 0)
     {
         //Solving error
     }
+}
+
+ExampleClassType ExampleClassType::operator++()
+{
+    for (auto element : _data)
+    {
+        element++;
+    }
+
+    return (ExampleClassType(_data));
+}
+
+ExampleClassType ExampleClassType::operator++(int ignoreMe)
+{
+    vector<int> temp = _data;
+
+    for (auto element : _data)
+    {
+        element++;
+    }
+
+    return (ExampleClassType(temp));
+}
+
+int& ExampleClassType::operator[](const int& index)
+{
+    return (_data[index]);
+}
+
+ExampleClassType& ExampleClassType::operator=(const ExampleClassType& rightObject)
+{
+    if (this != &rightObject)
+    {
+        _data = rightObject._data;
+    }
+
+    return (*this);
+};
+
+const ExampleClassType operator+(const ExampleClassType& firstObject, const ExampleClassType& secondObject)
+{
+    vector<int> resultData;
+    int smallerSize = (firstObject._data.size() < secondObject._data.size()) ? firstObject._data.size() : secondObject._data.size();
+    int biggerSize = (firstObject._data.size() > secondObject._data.size()) ? firstObject._data.size() : secondObject._data.size();
+
+    for (unsigned int index = 0; index < smallerSize; index++)
+    {
+        resultData.push_back((firstObject._data)[index] + (secondObject._data)[index]);
+    }
+
+    for (unsigned int index = smallerSize; index < biggerSize; index++)
+    {
+        if (firstObject._data.size() == biggerSize)
+        {
+            resultData.push_back((firstObject._data)[index]);
+        }
+        else
+        {
+            resultData.push_back((secondObject._data)[index]);
+        }
+    }
+
+    return (ExampleClassType(resultData));
+}
+
+istream& operator>>(istream& inputStream, ExampleClassType& object)
+{
+    for (auto element : object._data)
+    {
+        inputStream >> element;
+    }
+
+    return (inputStream);
+}
+
+ostream& operator<<(ostream& outputStream, const ExampleClassType& object)
+{
+    for (auto element : object._data)
+    {
+        outputStream << element << " ";
+    }
+
+    outputStream << endl;
+    return (outputStream);
 }
--- Cpp Convention/Cpp Convention - Scripts/Class/Class/ExampleClassType.h
+++ Cpp Convention/Cpp Convention - Scripts/Class/Class/ExampleClassType.h
@@ -7,13 +7,41 @@ of this type is data. The value is input and output as natural number. */
 class ExampleClassType
 {
     public:
+        ~ExampleClassType();
+        //Destructor comment
+
         ExampleClassType();
         //Default constructor comment
 
-        ExampleClassType(int dataValue);
+        ExampleClassType(const ExampleClassType& objectValue);
+        //Copy constructor comment
+
+        ExampleClassType(const vector<int>& dataValue);
         //Full constructor comment
 
+        ExampleClassType operator++();
+        //Operator overloading function of the increment operator (prefix version) comment
+
+        ExampleClassType operator++(int);
+        //Operator overloading function of the increment operator (postfix version) comment
+
+        int& operator[](const int& index);
+        //Operator overloading function of the array operator comment
+
+        ExampleClassType& operator=(const ExampleClassType& rightObject);
+        //Operator overloading function of the assignment operator comment
+
+        friend const ExampleClassType operator+(const ExampleClassType& firstObject, const ExampleClassType& secondObject);
+        //Operator overloading function of the addition operator comment
+
+        friend istream& operator>>(istream& inputStream, ExampleClassType& object);
+        //Operator overloading function of the extraction operator comment
+
+        friend ostream& operator<<(ostream& outputStream, const ExampleClassType& object);
+        //Operator overloading function of the insertion operator comment
+
     private:
-        int _data;
+        vector<int> _data;
+
 };
 #endif
